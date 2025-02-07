32b52f86dfa070fcbb0a2ae7ffc30ca649ce0b06
gushi-cookie <gushi-cookie@protonmail.com>
2025-02-07 16:08:34 +0000

    Installed and configured prettier with eslint.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import js from '@eslint/js';
 import ts from 'typescript-eslint';
 import jest from 'eslint-plugin-jest';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default ts.config(
   {
@@ -9,6 +10,7 @@ export default ts.config(
   js.configs.recommended,
   ...ts.configs.recommended,
   jest.configs['flat/recommended'],
+  prettierRecommended,
   {
     rules: {
       '@typescript-eslint/interface-name-prefix': 'off',
---
+++ prettier.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import("prettier").Config} */
+export default {
+    bracketSpacing: false,
+    singleQuote: true,
+    trailingComma: "all",
+    arrowParens: "avoid",
+};
