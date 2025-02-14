9c836007f53f41bdf1ea555aac10e465272427f2
gushi-cookie <gushi-cookie@protonmail.com>
2025-02-14 16:36:16 +0000

    Improved modules system.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export * as decorators from './decorators';
-export * as errors from './errors';
-export * as metadata from './errors';
-export * as resolver from './resolver';
+export * from './decorators';
+export * from './errors';
+export * from './metadata';
+export * from './resolver';
+export * as utils from './utils';
---
+++ src/utils/index.ts
@@ -0,0 +1,3 @@
+export * as collections from './collections.util';
+export * as environmentsSubstitutor from './environments-substitutor.util';
+export * as general from './general.util';
