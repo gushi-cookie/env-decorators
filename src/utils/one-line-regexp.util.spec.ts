ca7e8edc96b7d4db475cfe57daab93e2768bbbbb
gushi-cookie <gushi-cookie@protonmail.com>
2025-02-16 17:46:15 +0000

    Modified an util.

--- src/utils/one-line-regexp.util.spec.ts
+++ src/utils/one-line-regexp.util.spec.ts
@@ -1,40 +1,102 @@
 import {
   hasFlags,
-  areFlagsEscaped,
   extractFlags,
   parseOneLineRegExp,
 } from './one-line-regexp.util';
 
-test('should detect the flags construction: hasFlags()', () => {
-  expect(hasFlags('[Hh]ello[\\s]*[Ww]orld')).toBe(false);
-  expect(hasFlags('match[12345]word FLAGS:ig')).toBe(true);
-  expect(hasFlags('invalid_formatFLAGS:g')).toBe(false);
-});
+/**
+ * Test a regular expression that has `g` - `global flag`, on
+ * multiple input strings.
+ *
+ * Why? - Testing must be comprehensive. Since the `g` flag affects
+ * `lastIndex`, expecting that each next call is being performed on a
+ * same input, `lastIndex` must be reset for testing new inputs of the
+ * same `RegExp`.
+ *
+ * @param expression - pattern for testing inputs.
+ * @param pairs - set of values to test (keys) and expected outputs (values).
+ */
+function testGlobalRegExp(expression: RegExp, pairs: Map<string, boolean>) {
+  if (!expression.flags.includes('g')) {
+    throw new Error(
+      `Regular expression '${expression.toString()}' must have the 'g' flag.`,
+    );
+  }
 
-test('should detect if flags are escaped: areFlagsEscaped()', () => {
-  expect(areFlagsEscaped('no flags')).toBe(false);
-  expect(areFlagsEscaped('escaped\\ FLAGS:g')).toBe(true);
-  expect(areFlagsEscaped('not escaped FLAGS:gi')).toBe(false);
-  expect(areFlagsEscaped('escape escaped\\\\ FLAGS:giuvy')).toBe(false);
-  expect(areFlagsEscaped('odd escape\\\\\\ FLAGS:giuvy')).toBe(true);
+  for (let entry of pairs.entries()) {
+    expect(expression.test(entry[0])).toBe(entry[1]);
+    expression.lastIndex = 0;
+  }
+}
+
+test('should detect the flags construction: hasFlags()', () => {
+  // From examples:
+  expect(hasFlags('match[12345]word\\FLAGS:ig\\')).toBe(true);
+  expect(hasFlags('[Hh]ello[\\s]*[Ww]orld')).toBe(false);
+  expect(hasFlags('valid_format\\FLAGS:g\\')).toBe(true);
+  expect(hasFlags('invalid_format\\FLAG:123\\')).toBe(false);
+  expect(hasFlags('invalid_flags\\FLAGS:zxc\\')).toBe(false);
+
+  // Additional:
+  expect(hasFlags('\\FLAGS:gi\\ ')).toBe(false);
+  expect(hasFlags('\\FLAGS:gi\\ [abc123]')).toBe(false);
 });
 
 test('should extract flags: extractFlags()', () => {
-  expect(extractFlags('nothing')).toBe('');
-  expect(extractFlags('invalidFLAGS:gi')).toBe('');
-  expect(extractFlags('valid FLAGS:gims')).toBe('gims');
+  // From examples:
+  expect(extractFlags('match[12345]word\\FLAGS:ig\\')).toBe('ig');
+  expect(extractFlags('[Hh]ello[\\s]*[Ww]orld')).toBe('');
+  expect(extractFlags('valid_format\\FLAGS:dgimsuvy\\')).toBe('dgimsuvy');
+  expect(extractFlags('invalid_format\\FLAG:123\\')).toBe('');
+  expect(extractFlags('invalid_flags\\FLAGS:zxc\\')).toBe('');
+
+  // Additional:
+  expect(extractFlags('')).toBe('');
 });
 
 test('should parse one-line regular expression: parseOneLineRegExp()', () => {
-  const exp1 = parseOneLineRegExp('[Hh]ello[\\s]*[Ww]orld');
-  expect(exp1.flags).toBe('');
-  expect(exp1.test('hello world')).toBe(true);
+  // From examples:
+  const rx1 = parseOneLineRegExp('match[12345]word\\FLAGS:ig\\');
+  expect(rx1.flags).toMatch(/^[gi]{2}$/);
+  testGlobalRegExp(
+    rx1,
+    new Map<string, boolean>([
+      ['match5word', true],
+      ['MATCH1worD', true],
+      ['match6word', false],
+    ]),
+  );
 
-  const exp2 = parseOneLineRegExp('some_pattern FLAGS:giuy');
-  expect(exp2.flags).toMatch(/^[uyig]{4}$/);
-  expect(exp2.test('some_pattern')).toBe(true);
+  const rx2 = parseOneLineRegExp('[Hh]ello\\s*[Ww]orld');
+  expect(rx2.flags).toBe('');
+  expect(rx2.test('hello world')).toBe(true);
+  expect(rx2.test('HelloWorld')).toBe(true);
+  expect(rx2.test('hello_world')).toBe(false);
 
-  const exp3 = parseOneLineRegExp(' FLAGS:gi');
-  expect(exp3.flags).toMatch(/^[ig]{2}/);
-  expect(exp3.test('')).toBe(true);
+  const rx3 = parseOneLineRegExp('valid_format\\FLAGS:dgimsuy\\');
+  expect(rx3.flags).toMatch(/^[dgimsuy]{7}$/);
+  testGlobalRegExp(
+    rx3,
+    new Map<string, boolean>([
+      ['valid_format', true],
+      ['VALID_FORMAT', true],
+      ['FLAGS', false],
+    ]),
+  );
+
+  expect(() => parseOneLineRegExp('invalid_format\\FLAG:123\\')).toThrow(
+    SyntaxError,
+  );
+  expect(() => parseOneLineRegExp('invalid_flags\\FLAGS:zxc\\')).toThrow(
+    SyntaxError,
+  );
+
+  // Additional:
+  const rx4 = parseOneLineRegExp('some_pattern\\FLAGS:giuy\\');
+  expect(rx4.flags).toMatch(/^[uyig]{4}$/);
+  expect(rx4.test('some_pattern')).toBe(true);
+
+  const rx5 = parseOneLineRegExp('\\FLAGS:gi\\');
+  expect(rx5.flags).toMatch(/^[ig]{2}$/);
+  expect(rx5.test('')).toBe(true);
 });
--- src/utils/one-line-regexp.util.ts
+++ src/utils/one-line-regexp.util.ts
@@ -1,4 +1,4 @@
-export const flagsPattern = /\sFLAGS:[dgimsuvy]+$/;
+export const flagsPattern = /\\FLAGS:[dgimsuvy]+\\$/;
 
 /**
  * Check if an expression has the flags construction.
@@ -10,28 +10,6 @@ export function hasFlags(expression: string): boolean {
 }
 
 /**
- * Check if the flags construction is escaped. If there
- * is backslash series, before the flags construction,
- * then odd series of backslashes are considered escaped.
- *
- * @param expression - one-line expression to check.
- * @returns `true` if the flags construction is escaped.
- */
-export function areFlagsEscaped(expression: string): boolean {
-  let index = expression.search(flagsPattern);
-  if (index < 0) return false;
-
-  let backslashes = 0;
-  for (let i = index - 1; i >= 0; i--) {
-    if (expression.charAt(i) !== '\\') break;
-    backslashes++;
-  }
-
-  // If backslash series is odd then flags are escaped.
-  return Math.abs(backslashes % 2) === 1;
-}
-
-/**
  * Extract `RegExp` flags from a one-line regular expression.
  * @param expression - one-line expression to process.
  * @returns extracted flags or an empty string if not found.
@@ -39,42 +17,40 @@ export function areFlagsEscaped(expression: string): boolean {
 export function extractFlags(expression: string): string {
   let result = flagsPattern.exec(expression);
   if (result === null) return '';
-  return result[0].split(':')[1];
+  return result[0].split(':')[1].replace('\\', '');
 }
 
 /**
  * Parse a one-line regular expression from a string.
  *
  * One-line regexp is a format for describing `RegExp` types
- * in a single string. The format makes it possible to represent
- * both arguments of `RegExp` - `pattern` and `flags`, in a string.
+ * with a single string. The format makes it possible to represent
+ * both arguments of `RegExp`'s constructor - `pattern` and `flags`,
+ * in a string.
  *
- * Passed expression can be an ordinary `RegExp` pattern,
+ * Passed expression can be an ordinary `RegExp` pattern
  * without any constraint. But if add
- * `FLAGS:<flags>` construction to the end of this expression,
- * then it is interpreted as flags for the returned `RegExp`
- * instance. Patterns and flags constructions must be
- * delimited with a space.
- *
- * The flags construction can be escaped with `\` character, before the
- * space delimiter. If a backslashes series (without other characters)
- * is odd then the flags construction is escaped and ignored.
+ * `\FLAGS:<valid_flags>\` construction to the end of the expression's
+ * string, then it's interpreted as a flags argument to a new
+ * `RegExp` instance, that is getting prepared. Patterns and flags
+ * constructions shouldn't be delimited.
  *
  * Examples:
- * 1. `match[12345]word FLAGS:ig` becomes: `/match[12345]word/ig`
+ * 1. `match[12345]word\FLAGS:ig\` becomes: `/match[12345]word/gi`
  * 2. `[Hh]ello[\s]*[Ww]orld` becomes: `/[Hh]ello[\s]*[Ww]orld/`
- * 3. `valid_format FLAGS:g` becomes: `/valid_format/g`
- * 4. `invalid_formatFLAGS:g` becomes: `/invalid_formatFLAGS:g/`
- * 5. `escape_flags\ FLAGS:g` becomes: `/escape_flags FLAGS:g/`
- * 6. `escape_escape\\ FLAGS:g` becomes: `/escape_escape\/g`
- * 7. `odd_escape\\\ FLAGS:gi` becomes: `/odd_escape\\ FLAGS:gi/`
+ * 3. `valid_format\FLAGS:dgimsuvy\` becomes: `/valid_format/dgimsuvy`
+ * 4. `invalid_format\FLAG:123\` leads to a `SyntaxError`.
+ * 5. `invalid_flags\FLAGS:zxc\` leads to a `SyntaxError`.
+ *
+ * Note that these examples represent actual string values without
+ * escapes assumption.
  *
  * @param expression - one-line expression to parse.
  * @returns a new `RegExp` instance.
  */
 export function parseOneLineRegExp(expression: string): RegExp {
   let flags;
-  if (hasFlags(expression) && !areFlagsEscaped(expression)) {
+  if (hasFlags(expression)) {
     flags = extractFlags(expression);
     expression = expression.replace(flagsPattern, '');
   }
